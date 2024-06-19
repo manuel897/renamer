0a79918f4adcbb2e7cca9658a7cdd2f7c32dd30f
Manuel Mangalathu <manuelmangalathu@gmail.com>
2024-06-19 22:15:08 +0200

    Read input to select naming pattern

--- bin/renamer.dart
+++ bin/renamer.dart
@@ -13,14 +13,12 @@ ArgParser buildParser() {
       abbr: 'h',
       negatable: false,
       help: 'Print this usage information.',
-    )
-    ..addFlag(
+    )..addFlag(
       'verbose',
       abbr: 'v',
       negatable: false,
       help: 'Show additional command output.',
-    )
-    ..addFlag(
+    )..addFlag(
       'version',
       negatable: false,
       help: 'Print the tool version.',
@@ -28,8 +26,9 @@ ArgParser buildParser() {
 }
 
 void printUsage(ArgParser argParser) {
-  print('Usage: dart renamer.dart <path> <ignore item 1> <ignore item 2> ...');
-  print(argParser.usage);
+  stdout.writeln(
+      'Usage: dart renamer.dart <path> <ignore item 1> <ignore item 2> ...');
+  stdout.writeln(argParser.usage);
 }
 
 void main(List<String> arguments) {
@@ -44,7 +43,7 @@ void main(List<String> arguments) {
       return;
     }
     if (results.wasParsed('version')) {
-      print('renamer version: $version');
+      stdout.writeln('renamer version: $version');
       return;
     }
     if (results.wasParsed('verbose')) {
@@ -54,12 +53,13 @@ void main(List<String> arguments) {
     // Act on the arguments provided.
     // print('Positional arguments: ${results.rest}');
     if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
+      stdout.writeln('[VERBOSE] All arguments: ${results.arguments}');
     }
 
     if (results.arguments.isEmpty) {
       throw FormatException(
-          '1 positional argument is expected. ${results.arguments.length} found.');
+          '1 positional argument is expected. ${results.arguments
+              .length} found.');
     }
 
     var entryPath = results.arguments.first;
@@ -67,43 +67,53 @@ void main(List<String> arguments) {
     try {
       final entryDir = Directory(entryPath);
       if (!entryDir.existsSync()) {
-        print('Specified path $entryPath does not exist.');
+        stdout.writeln('Specified path $entryPath does not exist.');
         return;
       }
+
       var itemList = entryDir.listSync();
 
       final ignoreList = results.arguments.sublist(1);
-      print("Ignoring: $ignoreList");
+      stdout.writeln("Ignoring: $ignoreList");
 
       for (final item in ignoreList) {
         itemList.removeWhere((e) => e.path.contains(item));
       }
+
+      stdout.writeln("Please specify a naming pattern:");
+      stdout.writeln("1 = upper camel case (JaneDoe) [default]");
+      stdout.writeln("2 = upper snake case (Jane_Doe)");
+      final input = stdin.readLineSync();
+      var namingPattern = NamingPattern.upperCamelCase;
+      switch (input) {
+        case "1":
+          stdout.writeln('Selected upper camel case');
+          break;
+        case "2":
+          stdout.writeln('Selected upper snake case');
+          namingPattern = NamingPattern.upperSnakeCase;
+        default:
+          stdout.writeln('Selected upper camel case');
+          break;
+      }
+
       for (final FileSystemEntity item in itemList) {
         final name = RenamerHelper().getNameWithExtension(item.path);
-        //
+
         if (name != null) {
-          print(
-              "camel ${RenamerHelper().transformString(name, NamingPattern.upperCamelCase)}");
-          print(
-              "snake ${RenamerHelper().transformString(name, NamingPattern.upperSnakeCase)}");
+          stdout.writeln(
+              "$name -> ${RenamerHelper().transformString(
+                  name, namingPattern)}");
         }
-        //
-        // if (item is File) {
-        //   print("FILE: ${RenamerHelper().getNameWithExtension(item.path)}");
-        // } else if (item is Directory) {
-        //   print("DIR: ${RenamerHelper().getNameWithExtension(item.path)}");
-        // } else {
-        //   print("OTHER: ${RenamerHelper().getNameWithExtension(item.path)}");
-        // }
       }
     } on PathAccessException catch (e) {
-      print('Path $entryPath is inaccessible.');
-      print(e);
+      stderr.writeln('Path $entryPath is inaccessible.');
+      stderr.writeln(e);
     }
   } on FormatException catch (e) {
     // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
+    stderr.writeln(e.message);
+    stderr.writeln('');
     printUsage(argParser);
   }
 }
